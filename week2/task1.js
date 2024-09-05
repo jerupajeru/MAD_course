8717292dbd1ee72ab5a26de95e8f5983428710ab
jerupajeru <javeriainam51@gmail.com>
2024-09-05 11:43:07 +0500

    task of lecture 2

---
+++ week2/task1.js
@@ -0,0 +1,38 @@
+//Task 1
+function createArrayOfObjects() {
+    // Initialize an empty array
+    const arrayOfObjects = [];
+  
+    // Create objects
+    const object1 = {
+      name: 'javeria',
+      age: 24,
+      job: 'Engineer'
+    };
+  
+    const object2 = {
+      name: 'Ushna',
+      age: 25,
+      job: 'Designer'
+    };
+  
+    const object3 = {
+      name: 'Ayesha',
+      age: 35,
+      job: 'Manager'
+    };
+  
+    // Add objects to the array
+    arrayOfObjects.push(object1);
+    arrayOfObjects.push(object2);
+    arrayOfObjects.push(object3);
+  
+    // Return the array
+    return arrayOfObjects;
+  }
+  
+  // Call the function and store the result
+  const result = createArrayOfObjects();
+  
+  // Log the result to the console
+  console.log(result);
